9aa938a2c282b1b3fbf00d70d0f7f3681811ae79
Zablon Dawit <zabjd22@gmail.com>
2020-11-04 05:32:13 +0300

    Another commit

--- example-argparser/app.py
+++ example-argparser/app.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 import argparse
 
-parser = argparse.ArgumentParser()
-args = parser.parse_args()
+APP_DESCRIPTION = "A simple console program using argument parser module"
+
+def main():
+    global args
+    parser = argparse.ArgumentParser(description=APP_DESCRIPTION)
+    parser.add_argument('pos', nargs=1, help="positional argument")
+    parser.add_argument('--contains', help="Does it contain")
+    args = parser.parse_args()
+
+def action():
+    value = int(args.pos) * 2
+    print("Arguments contain: %d"%(value))
+
+if __name__ == "__main__":
+    main()
+    action()
